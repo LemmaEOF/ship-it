c4b9807d253bd7199710d0d1ce17ff62fb39d415
Meredith Espinosa <6722691+LemmaEOF@users.noreply.github.com>
2024-02-25 02:46:03 -0800

    commit Lambda Crimes because of class loading

--- src/main/java/dev/emi/shipit/ShipItClient.java
+++ src/main/java/dev/emi/shipit/ShipItClient.java
@@ -3,8 +3,13 @@ package dev.emi.shipit;
 import dev.emi.shipit.data.StationeryManager;
 import dev.emi.shipit.registry.ShipItPackets;
 import dev.emi.shipit.registry.ShipItScreens;
+import dev.emi.shipit.screen.LetterEditScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
 
 public class ShipItClient implements ClientModInitializer {
 
@@ -16,6 +21,9 @@ public class ShipItClient implements ClientModInitializer {
 				ShipItPackets.SYNC_STATIONERY,
 				(client, handler, buf, responseSender) -> StationeryManager.INSTANCE.readPacket(buf)
 		);
+	}
 
+	public static void openLetterScreen(PlayerEntity user, ItemStack stack, Hand hand) {
+		MinecraftClient.getInstance().setScreen(new LetterEditScreen(user, stack, hand));
 	}
 }
--- src/main/java/dev/emi/shipit/item/WritableLetterItem.java
+++ src/main/java/dev/emi/shipit/item/WritableLetterItem.java
@@ -1,5 +1,6 @@
 package dev.emi.shipit.item;
 
+import dev.emi.shipit.ShipItClient;
 import dev.emi.shipit.screen.LetterEditScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
@@ -12,9 +13,15 @@ import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.util.TriConsumer;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.function.Supplier;
+
 public class WritableLetterItem extends Item {
+	//yes, this is genuinely the best way to prevent a class-loading error for opening the edit screen without a packet
+	private static final Supplier<TriConsumer<PlayerEntity, ItemStack, Hand>> screenOpen = () -> ShipItClient::openLetterScreen;
+
 	public WritableLetterItem(Settings settings) {
 		super(settings);
 	}
@@ -23,8 +30,8 @@ public class WritableLetterItem extends Item {
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
 		ItemStack stack = user.getStackInHand(hand);
 		user.incrementStat(Stats.USED.getOrCreateStat(this));
-		if (world.isClient) {
-			MinecraftClient.getInstance().setScreen(new LetterEditScreen(user, stack, hand));
+		if (world.isClient()) {
+			screenOpen.get().accept(user, stack, hand);
 		}
 		return TypedActionResult.success(stack, world.isClient());
 	}
